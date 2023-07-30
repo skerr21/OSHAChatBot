f8f425818c619827676ccfb1a1d7ce4b987fcdb0
skerr21 <sirkerr@googlemail.com>
2023-07-30 16:16:00 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,42 +1,16 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, LineByLineTextDataset, TrainingArguments, Trainer, DataCollatorForLanguageModeling
-from torch.utils.data import Dataset
-import nltk
-import re
-nltk.download('punkt')
+
 # Load the pre-trained model and tokenizer
 model = AutoModelForCausalLM.from_pretrained('EleutherAI/gpt-neo-1.3B')
 tokenizer = AutoTokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B')
 tokenizer.pad_token = tokenizer.eos_token
-# Make sure gradient_checkpointing is enabled to save memory
-model.config.gradient_checkpointing = True
 
-class ConversationPairsDataset(Dataset):
-    def __init__(self, tokenizer, text, block_size):
-        self.tokenizer = tokenizer
-        self.block_size = block_size
-        
-        # Split the text into sentences
-        sentences = nltk.sent_tokenize(text)
-        
-        # Pair each sentence with the following sentence
-        self.examples = [(sentences[i], sentences[i + 1]) for i in range(len(sentences) - 1)]
-    
-    def __len__(self):
-        return len(self.examples)
-    
-    def __getitem__(self, i):
-        # Tokenize the pair of sentences and return as a single example
-        return self.tokenizer(self.examples[i][0] + ' ' + self.examples[i][1], truncation=True, max_length=self.block_size, padding='max_length')
 # Prepare the dataset
-with open("All About OSHA - all_about_OSHA.txt", 'r', encoding='utf-8') as f:
-    text = f.read()
-
-dataset = ConversationPairsDataset(
+dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
-    text=text,
-    block_size=128  # Reduce the block size if necessary
+    file_path="all_preprocessed_text.txt",
+    block_size=256,
 )
-
 
 # Prepare the data collator
 data_collator = DataCollatorForLanguageModeling(
@@ -47,14 +21,13 @@ data_collator = DataCollatorForLanguageModeling(
 training_args = TrainingArguments(
     output_dir="./fine_tuned_model",
     overwrite_output_dir=True,
-    num_train_epochs=1,
-    per_device_train_batch_size=1,  # Reduced batch size
-    gradient_accumulation_steps=16,  # Add gradient accumulation
-    fp16=True,  # Enable half-precision training
+    num_train_epochs=1,  # Increase the number of training epochs
+    per_device_train_batch_size=1,  # Increase the batch size
+    gradient_accumulation_steps=1,  # Adjust gradient accumulation steps according to your new batch size
+    fp16=True,  # Use full-precision training
     save_steps=10_000,
     save_total_limit=2,
 )
-
 
 # Train the model
 trainer = Trainer(
@@ -71,4 +44,3 @@ trainer.save_model()
 
 # Save the tokenizer
 tokenizer.save_pretrained("./fine_tuned_model")
-
