b61706d7237d836e3bdf8a93e053e98cf54be485
skerr21 <sirkerr@googlemail.com>
2023-07-31 16:02:36 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,8 +1,8 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, LineByLineTextDataset, TrainingArguments, Trainer, DataCollatorForLanguageModeling
+from transformers import AutoModelForCausalLM, AutoTokenizer, LineByLineTextDataset, TrainingArguments, Trainer, DataCollatorForLanguageModeling, AdamW, get_linear_schedule_with_warmup
 
 # Load the pre-trained model and tokenizer
-model = AutoModelForCausalLM.from_pretrained('EleutherAI/gpt-neo-1.3B')
-tokenizer = AutoTokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B')
+model = AutoModelForCausalLM.from_pretrained('gpt2')
+tokenizer = AutoTokenizer.from_pretrained('gpt2')
 tokenizer.pad_token = tokenizer.eos_token
 
 # Prepare the dataset
@@ -21,14 +21,13 @@ data_collator = DataCollatorForLanguageModeling(
 training_args = TrainingArguments(
     output_dir="./fine_tuned_model",
     overwrite_output_dir=True,
-    num_train_epochs=1,  # Increase the number of training epochs
-    per_device_train_batch_size=1,  # Increase the batch size
-    gradient_accumulation_steps=1,  # Adjust gradient accumulation steps according to your new batch size
-    fp16=True,  # Use full-precision training
+    num_train_epochs=6,  # Increase the number of training epochs
+    per_device_train_batch_size=4,  # Increase the batch size
+    # gradient_accumulation_steps=16,  # Adjust gradient accumulation steps according to your new batch size
+    fp16=False,  # Use full-precision training
     save_steps=10_000,
     save_total_limit=2,
 )
-
 # Train the model
 trainer = Trainer(
     model=model,
@@ -36,6 +35,17 @@ trainer = Trainer(
     data_collator=data_collator,
     train_dataset=dataset,
 )
+no_decay = ['bias', 'LayerNorm.weight']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+]
+optimizer = AdamW(optimizer_grouped_parameters, lr=1e-5)
+
+num_training_steps = len(trainer.get_train_dataloader()) * training_args.num_train_epochs
+lr_scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
+
+
 
 trainer.train()
 
