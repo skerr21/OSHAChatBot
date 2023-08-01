fd9c98ebd254ce433e1bb57b5e3eaefefa0700a8
skerr21 <sirkerr@googlemail.com>
2023-08-01 07:49:20 -0500

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -7,7 +7,7 @@ tokenizer = AutoTokenizer.from_pretrained('gpt2')
 tokenizer.pad_token = tokenizer.eos_token
 
 # Load and tokenize the text
-with open("preprocessed_text.txt", "r") as f:
+with open('cleaned_text.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # Tokenize the text and convert tokens to IDs
@@ -15,26 +15,30 @@ tokens = tokenizer.tokenize(text)
 token_ids = tokenizer.convert_tokens_to_ids(tokens)
 
 # Split the token IDs into chunks of fixed size
-block_size = 528
-tokenized_texts = [token_ids[i:i+block_size] for i in range(0, len(token_ids), block_size)]
+max_input_length = model.config.max_position_embeddings  # max input length for the GPT-2 model
+tokenized_texts = [token_ids[i:i+max_input_length] for i in range(0, len(token_ids), max_input_length)]
 
 # Prepare the dataset
 data = {'input_ids': tokenized_texts}
 dataset = Dataset.from_dict(data)
 
+# Check that the dataset is not empty
+if len(dataset) == 0:
+    raise ValueError("Dataset is empty.")
+
 # Prepare the data collator
 data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer, mlm=False,
+    tokenizer=tokenizer, mlm=False
 )
 
 # Set up the training arguments
 training_args = TrainingArguments(
-    output_dir="./fine_tuned_model",
+    output_dir='./results',
     overwrite_output_dir=True,
-    num_train_epochs=3,  # Increase the number of training epochs
-    per_device_train_batch_size=12,  # Increase the batch size
-    # gradient_accumulation_steps=16,  # Adjust gradient accumulation steps according to your new batch size
-    fp16=False,  # Use full-precision training
+    num_train_epochs=9,  # Increase the number of training epochs
+    per_device_train_batch_size=2,  # Increase the batch size
+    # gradient_accumulation_steps=8,  # Adjust gradient accumulation steps according to your new batch size
+    fp16=True,  # Use full-precision training
     save_steps=10_000,
     save_total_limit=2,
 )
@@ -57,19 +61,4 @@ trainer_temp = Trainer(
 num_training_steps = len(trainer_temp.get_train_dataloader()) * training_args.num_train_epochs
 lr_scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
 
-# Train the model
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    train_dataset=dataset,
-    optimizers=(optimizer, lr_scheduler),  # Pass the custom optimizer and lr_scheduler
-)
-
-trainer.train()
-
-# Save the model
-trainer.save_model()
-
-# Save the tokenizer
-tokenizer.save_pretrained("./fine_tuned_model")
+trainer_temp.train()
